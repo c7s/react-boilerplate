f251760da3b12e477c5bd2ab55d0b9d7a5039b72
Leonid Fenko <fenok2112@gmail.com>
2018-08-27 19:48:11 +0300

    ORG-512: Add commit message preparation and validation

---
+++ git/hooks/commit-msg.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const msgFilePath = path.resolve(process.env.GIT_PARAMS.split(' ')[0]);
+const msgFileContent = fs.readFileSync(msgFilePath, { encoding: 'utf-8' });
+
+const isMsgFormattedCorrectly = /^[A-Z]+-\d+: [A-Z]/.test(msgFileContent) || /^Merge/.test(msgFileContent);
+const isDoublePrefix = /^[A-Z]+-\d+: [A-Z]+-\d+/.test(msgFileContent);
+
+if (isDoublePrefix) {
+    terminateWithWarning("You've specified prefix in wrong format while being on task branch");
+} else if (!isMsgFormattedCorrectly) {
+    terminateWithWarning(`Message must look like 'KP-00: Makes stuff' or 'Merge ...', instead got: ${msgFileContent}`);
+}
+
+/**
+ * @param {string} warning
+ */
+function terminateWithWarning(warning) {
+    console.warn(`[WARN] ${warning}`);
+    process.exit(1);
+}
---
+++ git/hooks/prepare-commit-msg.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const {execSync} = require('child_process');
+
+const msgPrefixMatch = /([A-Z]+-\d+)/.exec(execSync('git symbolic-ref --short HEAD', { encoding: 'utf-8' }));
+
+const msgFilePath = path.resolve(process.env.GIT_PARAMS.split(' ')[0]);
+const msgFileContent = fs.readFileSync(msgFilePath, { encoding: 'utf-8' });
+
+const isAlreadyWithPrefix = /^[A-Z]+-\d+: /.test(msgFileContent);
+const isMerge = /^Merge/.test(msgFileContent);
+
+let msgPrefix = '';
+if (msgPrefixMatch && !isAlreadyWithPrefix && !isMerge) {
+    msgPrefix = `${msgPrefixMatch[1]}: `;
+} else if (msgPrefixMatch && isAlreadyWithPrefix) {
+    console.warn(`[WARN] You shouldn't specify prefix while being on task branch`);
+}
+
+fs.writeFileSync(msgFilePath, `${msgPrefix}${msgFileContent}`);
