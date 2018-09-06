f0b500189fcfe47fb119e662b5d4000b594ea08d
Leonid Fenko <fenok2112@gmail.com>
2018-09-06 18:09:34 +0300

    ORG-512: Add meta tags and lang attribute

--- frontend/src/client/Html.tsx
+++ frontend/src/client/Html.tsx
@@ -8,8 +8,17 @@ export interface HtmlProps {
 
 export const Html: React.StatelessComponent<HtmlProps> = ({ styleTags, content, state }) => {
     return (
-        <html>
-            <head dangerouslySetInnerHTML={{ __html: `<title>App</title>${styleTags}` }} />
+        <html lang="ru">
+            <head
+                dangerouslySetInnerHTML={{
+                    __html: `<meta charset="utf-8">
+                             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+                             <meta name="mobile-web-app-capable" content="yes">
+                             <meta name="apple-mobile-web-app-capable" content="yes">
+                             <title>App</title>
+                             ${styleTags}`,
+                }}
+            />
             <body>
                 <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
                 <script
