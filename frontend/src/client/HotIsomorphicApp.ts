5f116ab5209b7a17b236508469fca25381cd37ab
Leonid Fenko <fenok2112@gmail.com>
2018-09-14 20:01:35 +0300

    ORG-512: Add bad design explanation

--- frontend/src/client/HotIsomorphicApp.ts
+++ frontend/src/client/HotIsomorphicApp.ts
@@ -1,6 +1,8 @@
 import { hot } from 'react-hot-loader';
 import { IsomorphicApp } from './IsomorphicApp';
 
-// 'hot' HOC breaks apollo SSR, so separate client-side HotIsomorphicApp is needed
-
+/**
+ * HOC 'hot' breaks apollo SSR and requires to have it in separate (from client entry point) file.
+ * It's also impossible to move it to ./IsomorphicApp for whatever reasons (it breaks tslint).
+ */
 export const HotIsomorphicApp = hot(module)(IsomorphicApp);
