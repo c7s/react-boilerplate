c476f684166c0fda54b6aec28061698d824d513d
Leonid Fenko <fenok2112@gmail.com>
2018-09-21 02:54:24 +0300

    ORG-512: Add line length validation (#9)

--- frontend/git/hooks/commit-msg.js
+++ frontend/git/hooks/commit-msg.js
@@ -6,11 +6,14 @@ const msgFileContent = fs.readFileSync(msgFilePath, { encoding: 'utf-8' });
 
 const isMsgFormattedCorrectly = /^[A-Z]+-\d+: [A-Z]/.test(msgFileContent) || /^Merge/.test(msgFileContent);
 const isDoublePrefix = /^[A-Z]+-\d+: [A-Z]+-\d+/.test(msgFileContent);
+const isLongLines = /^.{73,}$/m.test(msgFileContent);
 
 if (isDoublePrefix) {
     terminateWithWarning("You've specified prefix in wrong format while being on task branch");
 } else if (!isMsgFormattedCorrectly) {
     terminateWithWarning(`Message must look like 'KP-00: Makes stuff' or 'Merge ...', instead got: ${msgFileContent}`);
+} else if (isLongLines) {
+    terminateWithWarning('Message lines must not exceed 72 characters');
 }
 
 /**
