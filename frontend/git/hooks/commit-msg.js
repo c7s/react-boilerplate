7b8e006026633aff876c0a4fd4df53ab9653f173
Leonid Fenko <fenok2112@gmail.com>
2018-09-21 03:06:29 +0300

    ORG-512: Add blank line validation (#9)

--- frontend/git/hooks/commit-msg.js
+++ frontend/git/hooks/commit-msg.js
@@ -7,6 +7,8 @@ const msgFileContent = fs.readFileSync(msgFilePath, { encoding: 'utf-8' });
 const isMsgFormattedCorrectly = /^[A-Z]+-\d+: [A-Z]/.test(msgFileContent) || /^Merge/.test(msgFileContent);
 const isDoublePrefix = /^[A-Z]+-\d+: [A-Z]+-\d+/.test(msgFileContent);
 const isLongLines = /^.{73,}$/m.test(msgFileContent);
+const isHasBody = /^.+\n+.+/.test(msgFileContent);
+const isBlankLineAfterSubject = /^.+\n\n.+/.test(msgFileContent);
 
 if (isDoublePrefix) {
     terminateWithWarning("You've specified prefix in wrong format while being on task branch");
@@ -14,6 +16,8 @@ if (isDoublePrefix) {
     terminateWithWarning(`Message must look like 'KP-00: Makes stuff' or 'Merge ...', instead got: ${msgFileContent}`);
 } else if (isLongLines) {
     terminateWithWarning('Message lines must not exceed 72 characters');
+} else if (isHasBody && !isBlankLineAfterSubject) {
+    terminateWithWarning('There must be exactly one blank line between subject and body');
 }
 
 /**
