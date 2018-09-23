5f7c31ae2403ba9d7c3661bb86dac9b78d7e05bb
Leonid Fenko <fenok2112@gmail.com>
2018-09-23 20:09:37 +0300

    ORG-512: Remove 'any' from globals

--- frontend/@types/globals.d.ts
+++ frontend/@types/globals.d.ts
@@ -1,7 +1,7 @@
 declare const GRAPHQL_ENDPOINT: string;
 declare const GITHUB_TOKEN: string;
-declare const APOLLO_STATE: any;
-declare const REDUX_STATE: any;
+declare const APOLLO_STATE: { [key: string]: { [key: string]: object } };
+declare const REDUX_STATE: object;
 declare const CLIENT_BUNDLE_NAME: string;
 declare const STATIC_DIRECTORY_NAME: string;
 declare const WEB_MANIFEST_PATH: string;
