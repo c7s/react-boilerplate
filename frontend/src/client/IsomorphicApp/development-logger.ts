52538b8d1f2828ec3d2006bbea87353765bcbaa9
Leonid Fenko <fenok2112@gmail.com>
2018-09-26 00:10:25 +0300

    ORG-512: Restrict development logger to browser only

--- frontend/src/client/IsomorphicApp/development-logger.ts
+++ frontend/src/client/IsomorphicApp/development-logger.ts
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' && !SSR_MODE) {
     console.log(`SSR_ERROR: ${global.SSR_ERROR ? global.SSR_ERROR.message : 'none'}
 BUILD_TIME: ${new Date(BUILD_TIMESTAMP).toLocaleString()}
 GRAPHQL_ENDPOINT: ${GRAPHQL_ENDPOINT}`);
