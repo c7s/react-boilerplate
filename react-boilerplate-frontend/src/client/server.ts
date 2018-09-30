27b92d14e118395fe33e9b188ae9ca029d1f6298
Leonid Fenko <fenok2112@gmail.com>
2018-09-30 23:14:50 +0300

    ORG-512: Refactor server (readability)

--- react-boilerplate-frontend/src/client/server.ts
+++ react-boilerplate-frontend/src/client/server.ts
@@ -36,72 +36,83 @@ interface FrontendServerStats {
     reactLoadableStats: ReactLoadableStats;
 }
 
+interface RouterContext {
+    url?: string;
+}
+
 export default function serverRenderer(
     stats: WebpackHotServerMiddlewareStats | FrontendServerStats,
     link?: ApolloLink,
 ) {
-    let reactLoadableStats: ReactLoadableStats;
-    if (!isWebpackHotServerMiddlewareStats(stats)) {
-        reactLoadableStats = stats.reactLoadableStats;
-    } else {
-        reactLoadableStats = convertWebpackHotServerMiddlewareStatsToReactLoadableStats(stats);
-    }
-
     return (req: Request, res: Response) => {
         if (req.path === WEB_MANIFEST_PATH) {
-            res.status(200)
-                .header('Content-Type', 'application/manifest+json')
-                .send(JSON.stringify(webManifest));
+            sendWebManifest(res);
         } else if (req.path === BROWSER_CONFIG_PATH) {
-            res.status(200)
-                .header('Content-Type', 'application/xml')
-                .send(browserConfig);
+            sendBrowserConfig(res);
         } else {
-            const client = IsomorphicApolloClient.getClient({ fetch, link });
-            const store = IsomorphicStore.getStore();
-            const context: { url?: string } = {};
-            const sheet = new ServerStyleSheet();
-            const modules: string[] = [];
-
-            const App = React.createElement(IsomorphicApp, {
-                client,
-                store,
-                modules,
-                context,
-                location: req.url,
-            });
-
-            getDataFromTree(App)
-                .then(() => {
-                    const content = renderToString(sheet.collectStyles(App));
-
-                    if (context.url) {
-                        res.status(301)
-                            .header('Location', context.url)
-                            .send();
-                    } else {
-                        const html = React.createElement(Html, {
-                            content,
-                            helmet: Helmet.renderStatic(),
-                            styleTags: sheet.getStyleTags(),
-                            spriteContent: sprite.stringify(),
-                            apolloState: client.extract(),
-                            reduxState: store.getState(),
-                            bundles: getNormalizedBundles(reactLoadableStats, modules),
-                        });
-
-                        res.status(200).send(`<!doctype html>\n${renderToStaticMarkup(html)}`);
-                    }
-                })
-                .catch((error: Error) => {
-                    const html = React.createElement(Html, {
-                        ssrError: error,
-                    });
-
-                    res.status(200).send(`<!doctype html>\n${renderToStaticMarkup(html)}`);
-                });
+            sendHtmlOrRedirect(req, res, getReactLoadableStats(stats), link);
         }
     };
+}
+
+function sendHtmlOrRedirect(req: Request, res: Response, reactLoadableStats: ReactLoadableStats, link?: ApolloLink) {
+    const client = IsomorphicApolloClient.getClient({ fetch, link });
+    const store = IsomorphicStore.getStore();
+    const context: RouterContext = {};
+    const sheet = new ServerStyleSheet();
+    const modules: string[] = [];
+
+    const App = React.createElement(IsomorphicApp, { client, store, modules, context, location: req.url });
+
+    getDataFromTree(App)
+        .then(() => {
+            const content = renderToString(sheet.collectStyles(App));
+
+            if (context.url) {
+                sendRedirect(res, context);
+            } else {
+                const html = React.createElement(Html, {
+                    content,
+                    helmet: Helmet.renderStatic(),
+                    styleTags: sheet.getStyleTags(),
+                    spriteContent: sprite.stringify(),
+                    apolloState: client.extract(),
+                    reduxState: store.getState(),
+                    bundles: getNormalizedBundles(reactLoadableStats, modules),
+                });
+
+                sendHtml(res, html);
+            }
+        })
+        .catch((error: Error) => {
+            const html = React.createElement(Html, {
+                ssrError: error,
+            });
+
+            sendHtml(res, html);
+        });
+}
+
+function sendHtml(res: Response, html: React.ReactElement<{}>) {
+    res.status(200).send(`<!doctype html>\n${renderToStaticMarkup(html)}`);
+}
+
+function sendWebManifest(res: Response) {
+    res.status(200)
+        .header('Content-Type', 'application/manifest+json')
+        .send(JSON.stringify(webManifest));
+}
+
+function sendBrowserConfig(res: Response) {
+    res.status(200)
+        .header('Content-Type', 'application/xml')
+        .send(browserConfig);
+}
+
+function sendRedirect(res: Response, context: RouterContext) {
+    res.status(301)
+        .header('Location', context.url)
+        .send();
 }
 
 function getNormalizedBundles(reactLoadableStats: ReactLoadableStats, modules: string[]): ReactLoadableBundle[] {
@@ -109,6 +120,17 @@ function getNormalizedBundles(reactLoadableStats: ReactLoadableStats, modules: s
         getBundles(reactLoadableStats, uniq(modules)),
         bundle => !bundle.file.includes('.js.map'),
     ) as ReactLoadableBundle[]; // Official type definition error
+}
+
+function getReactLoadableStats(stats: WebpackHotServerMiddlewareStats | FrontendServerStats): ReactLoadableStats {
+    let reactLoadableStats: ReactLoadableStats;
+    if (!isWebpackHotServerMiddlewareStats(stats)) {
+        reactLoadableStats = stats.reactLoadableStats;
+    } else {
+        reactLoadableStats = convertWebpackHotServerMiddlewareStatsToReactLoadableStats(stats);
+    }
+
+    return reactLoadableStats;
 }
 
 function isWebpackHotServerMiddlewareStats(
