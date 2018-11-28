43648e66ae95ef151f7252097af86c5295b58767
Leonid Fenko <fenok2112@gmail.com>
2018-11-28 14:18:56 +0300

    ORG-550: Improve log for yarn.lock and types check (#70)

--- react-boilerplate-frontend/tools/git/hooks/check-types.js
+++ react-boilerplate-frontend/tools/git/hooks/check-types.js
@@ -1,3 +1,9 @@
 const { execSync } = require('child_process');
 
-execSync('tsc --project tsconfig.json', { encoding: 'utf-8' });
+try {
+    // https://stackoverflow.com/a/31104898
+    execSync('tsc --project tsconfig.json', { stdio: [0, 1, 2] });
+} catch (error) {
+    // Prevent unrelated error log
+    process.exit(1);
+}
