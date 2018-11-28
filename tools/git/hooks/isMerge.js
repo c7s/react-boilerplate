c5fd98d667a3042f1311901aecf1416bbe7c2615
Leonid Fenko <fenok2112@gmail.com>
2018-11-28 20:00:58 +0300

    ORG-550: Move repository-specific hooks to root (#70)

---
+++ tools/git/hooks/isMerge.js
@@ -0,0 +1,11 @@
+const { execSync } = require("child_process");
+
+let isMerge = false;
+try {
+  execSync("git rev-parse -q --verify MERGE_HEAD", { encoding: "utf-8" });
+  isMerge = true;
+} catch (error) {
+  // Okay, it's not merge
+}
+
+module.exports = { isMerge };
---
+++ tools/git/hooks/prepare-commit-msg.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+const { isMerge } = require("./isMerge");
+
+try {
+  const msgPrefixMatch = /([A-Z]+-\d+)/.exec(
+    execSync("git symbolic-ref --short HEAD", { encoding: "utf-8" })
+  );
+
+  const messageFileName = process.env.HUSKY_GIT_PARAMS.split(" ")[0];
+  const msgFilePath = path.resolve("..", messageFileName);
+
+  const msgFileContent = fs.readFileSync(msgFilePath, { encoding: "utf-8" });
+
+  const isAlreadyWithPrefix = /^[A-Z]+-\d+: /.test(msgFileContent);
+
+  let msgPrefix = "";
+  if (msgPrefixMatch && !isAlreadyWithPrefix && !isMerge) {
+    msgPrefix = `${msgPrefixMatch[1]}: `;
+  } else if (msgPrefixMatch && isAlreadyWithPrefix) {
+    console.warn(
+      `[WARN] You shouldn't specify prefix while being on task branch`
+    );
+  }
+
+  fs.writeFileSync(msgFilePath, `${msgPrefix}${msgFileContent}`);
+} catch (error) {
+  console.warn(
+    "[WARN] Something unusual is happening, commit message will not be prepared"
+  );
+}
