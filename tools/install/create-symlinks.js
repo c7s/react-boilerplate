adcf043f0270896bdf270a6e8db7b0af602314dc
Leonid Fenko <fenok2112@gmail.com>
2018-11-29 13:10:39 +0300

    ORG-550: Add symlinks creation to root (#70)

---
+++ tools/install/create-symlinks.js
@@ -0,0 +1,11 @@
+const createSymlink = require('create-symlink');
+const fs = require('fs');
+const path = require('path');
+
+/** Hack to provide prettier.cmd on linux and therefore use shareable prettier watcher */
+if (!fs.existsSync(path.resolve(process.cwd(), 'node_modules/.bin/prettier.cmd'))) {
+    createSymlink(
+        path.resolve(process.cwd(), 'node_modules/.bin/prettier'),
+        path.resolve(process.cwd(), 'node_modules/.bin/prettier.cmd')
+    );
+}
