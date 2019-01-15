9b5414c861f3fe75e08de9ca9cc105638e30d987
Andrey Tsapko <tsapko3628@gmail.com>
2019-01-15 20:12:42 +0300

    ORG-551 fix user entity from sequelize to typeorm entity

--- packages/backend/src/infra/database/models/User.ts
+++ packages/backend/src/infra/database/models/User.ts
@@ -1,5 +1,24 @@
-export function User(sequelize, DataTypes) {
-  return sequelize.define('user', {
-    name: DataTypes.STRING,
-  });
-};
+import {
+    Entity,
+    BaseEntity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Column('varchar')
+    public name: string;
+
+    @CreateDateColumn()
+    public createdAt: Date;
+
+    @UpdateDateColumn()
+    public updatedAt: Date;
+}
--- packages/backend/src/infra/database/models/index.ts
+++ packages/backend/src/infra/database/models/index.ts
@@ -1,16 +1,1 @@
-import { ModelsLoader } from '../../sequelize';
-import Sequelize from 'sequelize';
-const { db: config } = require('../../../../config');
-
-if (!config) {
-    throw new Error('Database configuration not found, disabling database.');
-}
-
-const sequelize = new Sequelize(config);
-
-export const { database } = ModelsLoader.load({
-    sequelize,
-    baseFolder: __dirname
-});
-
 export { User } from './User';
