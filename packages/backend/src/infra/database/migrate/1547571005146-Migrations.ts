2fca60fdbc530a89f70470a4c43059905708ed54
Andrey Tsapko <tsapko3628@gmail.com>
2019-01-15 19:59:30 +0300

    ORG-551 change sequelize migrations to typeorm

---
+++ packages/backend/src/infra/database/migrate/1547571005146-Migrations.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Migrations1547571005146 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
