bb04deb67f34dcf252ccd6275783a8d7d22c1910
Leonid Fenko <fenok2112@gmail.com>
2019-01-25 17:29:14 +0300

    ORG-550: Remove any

--- packages/frontend/src/client/modules/common/components/IsomorphicApp/console-enhancers/ConsoleMonkeyPatcher.ts
+++ packages/frontend/src/client/modules/common/components/IsomorphicApp/console-enhancers/ConsoleMonkeyPatcher.ts
@@ -1,19 +1,21 @@
 /** Groups [HMR] logs for clear console */
 class ConsoleMonkeyPatcher {
     isHmrGroup: boolean = false;
-    console: Console;
+    console: Console & { [index: string]: (...args: unknown[]) => void };
 
     constructor(console: Console) {
-        this.console = { ...console };
+        this.console = { ...console } as Console & { [index: string]: (...args: unknown[]) => void };
 
         Object.keys(console).forEach(consoleFunctionName => {
-            (console as any)[consoleFunctionName] = this.patchedConsoleFunction.bind(this, consoleFunctionName);
+            (console as Console & { [index: string]: (...args: unknown[]) => void })[
+                consoleFunctionName
+            ] = this.patchedConsoleFunction.bind(this, consoleFunctionName);
         });
     }
 
     private patchedConsoleFunction(consoleFunctionName: string, ...args: unknown[]) {
         this.updateHrmGroupState(String(args[0]));
-        (this.console as any)[consoleFunctionName](...args);
+        this.console[consoleFunctionName](...args);
     }
 
     private updateHrmGroupState(message: string) {
