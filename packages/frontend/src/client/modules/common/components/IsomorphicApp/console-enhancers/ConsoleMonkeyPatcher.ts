83256a79dcf94ef60aa5cfb4963f3a43c4d0e1d3
Leonid Fenko <fenok2112@gmail.com>
2019-01-25 16:54:05 +0300

    ORG-550: Apply console patcher to all functions

--- packages/frontend/src/client/modules/common/components/IsomorphicApp/console-enhancers/ConsoleMonkeyPatcher.ts
+++ packages/frontend/src/client/modules/common/components/IsomorphicApp/console-enhancers/ConsoleMonkeyPatcher.ts
@@ -1,29 +1,27 @@
-import autobind from 'autobind-decorator';
-
 /** Groups [HMR] logs for clear console */
 class ConsoleMonkeyPatcher {
     isHmrGroup: boolean = false;
-    log: Console['log'];
+    console: Console;
 
     constructor(console: Console) {
-        /* tslint:disable no-console */
-        this.log = console.log;
-        console.log = this.smartLog;
-        /* tslint:enable */
+        this.console = { ...console };
+
+        Object.keys(console).forEach(consoleFunctionName => {
+            (console as any)[consoleFunctionName] = this.patchedConsoleFunction.bind(this, consoleFunctionName);
+        });
     }
 
-    @autobind
-    private smartLog(message: string, ...args: any[]) {
-        this.updateHrmGroupState(message);
-        this.log(message, ...args);
+    private patchedConsoleFunction(consoleFunctionName: string, ...args: unknown[]) {
+        this.updateHrmGroupState(String(args[0]));
+        (this.console as any)[consoleFunctionName](...args);
     }
 
     private updateHrmGroupState(message: string) {
         if (this.isHmrGroup && !message.includes('[HMR]')) {
-            console.groupEnd();
+            this.console.groupEnd();
             this.isHmrGroup = false;
         } else if (!this.isHmrGroup && message.includes('[HMR]')) {
-            console.groupCollapsed('[HMR] Group');
+            this.console.groupCollapsed('[HMR] Group');
             this.isHmrGroup = true;
         }
     }
