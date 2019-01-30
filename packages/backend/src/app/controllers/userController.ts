579fcc24f8f538603d9cb7f785b1282c9842950d
Andrey Tsapko <tsapko3628@gmail.com>
2019-01-30 17:28:18 +0300

    ORG-551: Add delete method in user controller

--- packages/backend/src/app/controllers/userController.ts
+++ packages/backend/src/app/controllers/userController.ts
@@ -3,6 +3,7 @@ import {
     Get,
     Param,
     OnUndefined,
+    Delete
 } from 'routing-controllers';
 
 import { Container } from '../../container';
@@ -11,7 +12,7 @@ import { container } from '../../containerCreate';
 @JsonController('/user')
 export class UserController {
 
-    private readonly repository: Container['usersRepository'] = container.resolve('usersRepository');
+    private readonly repository = container.resolve<Container['usersRepository']>('usersRepository');
 
     @Get('/')
     public async list(): Promise<any[]> {
@@ -22,5 +23,11 @@ export class UserController {
     @OnUndefined(404)
     public async one(@Param('id') id: number): Promise<any> {
         return this.repository.findOne(id);
+    }
+
+    @Delete('/:id')
+    @OnUndefined(204)
+    public async delete(@Param('id') id: number): Promise<any> {
+        return this.repository.delete(id);
     }
 }
