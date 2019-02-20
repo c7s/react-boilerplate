7c7527e3781aebf3fac75ef0bc36534afc87a097
Leonid Fenko <fenok2112@gmail.com>
2019-02-20 20:19:51 +0300

    ORG-550: Enable momentum scrolling of modal

--- packages/frontend/src/client/modules/common/components/Modal/ModalTemplate.tsx
+++ packages/frontend/src/client/modules/common/components/Modal/ModalTemplate.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import ResponsiveModal from 'react-responsive-modal';
+import { createGlobalStyle } from 'styled-components';
 import { CommonProps } from '../../types/CommonProps';
 
 interface Props extends ExtractProps<typeof ResponsiveModal>, CommonProps {}
 
-const ModalTemplate: React.FC<Props> = props => <ResponsiveModal {...props} />;
+const ModalTemplate: React.FC<Props> = props => (
+    <React.Fragment>
+        <ResponsiveModalGlobalStyle />
+        <ResponsiveModal {...props} classNames={{ overlay: 'responsive-modal-overlay' }} />
+    </React.Fragment>
+);
+
+const ResponsiveModalGlobalStyle = createGlobalStyle`
+    .responsive-modal-overlay {
+        -webkit-overflow-scrolling: touch;
+    }
+`;
 
 export { ModalTemplate, Props };
