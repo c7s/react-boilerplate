dd17b5c1aa46cd3d2bc18ae6bf324b337bea1c7c
Leonid Fenko <fenok2112@gmail.com>
2019-03-19 14:44:06 +0300

    ORG-550: Check in apollo types to enable their re-export (#95)

---
+++ packages/frontend/src/client/modules/development/pages/DevelopmentPage/ApolloTypes/License.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: License
+// ====================================================
+
+export interface License {
+    __typename: 'License';
+    /**
+     * Customary short name if applicable (e.g, GPLv3)
+     */
+    nickname: string | null;
+    /**
+     * A human-readable description of the license
+     */
+    description: string | null;
+}
---
+++ packages/frontend/src/client/modules/development/pages/DevelopmentPage/ApolloTypes/Licenses.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: Licenses
+// ====================================================
+
+export interface Licenses_licenses {
+    __typename: 'License';
+    /**
+     * Customary short name if applicable (e.g, GPLv3)
+     */
+    nickname: string | null;
+    /**
+     * A human-readable description of the license
+     */
+    description: string | null;
+}
+
+export interface Licenses {
+    /**
+     * Return a list of known open source licenses
+     */
+    licenses: (Licenses_licenses | null)[];
+}
