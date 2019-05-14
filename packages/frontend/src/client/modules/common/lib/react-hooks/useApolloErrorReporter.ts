668b1ffee06706ac01d0a4b2410437313526a331
Leonid Fenko <fenok2112@gmail.com>
2019-05-14 20:29:10 +0300

    ORG-550: Add useApolloErrorReporter hook (#114)

---
+++ packages/frontend/src/client/modules/common/lib/react-hooks/useApolloErrorReporter.ts
@@ -0,0 +1,45 @@
+import { ApolloError } from 'apollo-client';
+import * as React from 'react';
+import { onMessageAdd } from '../../store/actions';
+import { IsomorphicStore } from '../IsomorphicStore';
+
+interface Options {
+    disabled?: boolean;
+    ignore?: string[]; // TODO: ServerError enum
+}
+
+/** Report non-network errors via Notificator (network errors are handled automatically) */
+export function useApolloErrorReporter(result: { error?: ApolloError; loading: boolean }, opt: Options = {}) {
+    const disabled = Boolean(opt.disabled);
+    const ignore = opt.ignore || [];
+
+    const [wasShownFromLastLoading, setWasShownFromLastLoading] = React.useState(false);
+
+    React.useEffect(
+        () => {
+            if (
+                !disabled &&
+                !wasShownFromLastLoading &&
+                result.error &&
+                !result.error.networkError &&
+                !includesOnly(result.error, ignore)
+            ) {
+                IsomorphicStore.getStore().dispatch(onMessageAdd(result.error));
+                setWasShownFromLastLoading(true);
+            }
+
+            if (result.loading) {
+                setWasShownFromLastLoading(false);
+            }
+        },
+        [result, wasShownFromLastLoading, opt],
+    );
+}
+
+export function includesOnly(apolloError: ApolloError, codeList: string[] /* TODO: ServerError enum */) {
+    if (!apolloError.networkError) {
+        return apolloError.graphQLErrors.every(graphqlError => codeList.includes(graphqlError.message));
+    }
+
+    return false;
+}
