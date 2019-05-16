998e3151d460b2f2530fef2cf5edee8e37b17f03
Leonid Fenko <fenok2112@gmail.com>
2019-05-16 17:40:54 +0300

    ORG-550: Prettify all files

--- packages/frontend/src/client/modules/common/components/Header/HeaderTemplate.tsx
+++ packages/frontend/src/client/modules/common/components/Header/HeaderTemplate.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { CommonProps } from "../../types/CommonProps";
+import { CommonProps } from '../../types/CommonProps';
 
 interface Props extends CommonProps {}
 
