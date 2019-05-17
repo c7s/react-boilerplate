0b17461e030a28aa5d87be5e8a8f31b950d1471e
Leonid Fenko <fenok2112@gmail.com>
2019-05-17 20:00:15 +0300

    ORG-550: Add error handler

--- packages/frontend-server/src/index.js
+++ packages/frontend-server/src/index.js
@@ -12,20 +12,25 @@ try {
 
     // Create public files and serve them from '/' with correct headers
     Object.values(publicFiles).forEach(({ path: filePath, content, contentType }) => {
-        fs.writeFileSync(path.resolve(`./node_modules/frontend/dist/public${filePath}`), content);
+        try {
+            fs.writeFileSync(path.resolve(`./node_modules/frontend/dist/public${filePath}`), content);
 
-        app.use(filePath, (req, res, next) => {
-            res.header('Content-Type', contentType);
-            next();
-        });
+            app.use(filePath, (req, res, next) => {
+                res.header('Content-Type', contentType);
+                next();
+            });
+        } catch (error) {
+            console.warn(`[WARN] Unable to create file at ${filePath}`);
+            console.warn(error);
+        }
     });
 
     app.use(express.static('./node_modules/frontend/dist/public'));
     app.use('/static', express.static('./node_modules/frontend/dist/static'));
     app.use(serverRenderer({ reactLoadableStats }));
-} catch (e) {
-    if (e.code !== 'MODULE_NOT_FOUND') {
-        throw e;
+} catch (error) {
+    if (error.code !== 'MODULE_NOT_FOUND') {
+        throw error;
     }
     // We're good, frontend (server renderer) is optional dependency
 }
