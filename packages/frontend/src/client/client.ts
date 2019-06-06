fbd5a5aa2dcacad85738d5918079efa151aa04e1
Leonid Fenko <fenok2112@gmail.com>
2019-06-06 14:59:57 +0300

    ORG-550: Fix render or hydrate choice

--- packages/frontend/src/client/client.ts
+++ packages/frontend/src/client/client.ts
@@ -5,13 +5,19 @@ import './isomorphic-globals-init';
 import { IsomorphicApp } from './modules/common/components/IsomorphicApp';
 import { IsomorphicApolloClient } from './modules/common/lib/IsomorphicApolloClient';
 
-const reactHydrateOrRender = global.SSR_ERROR ? ReactDOM.render : ReactDOM.hydrate;
+const rootElement = document.getElementById('root');
 
-Loadable.preloadReady().then(() => {
-    reactHydrateOrRender(
-        React.createElement(IsomorphicApp, {
-            client: IsomorphicApolloClient.getClient(),
-        }),
-        document.getElementById('root'),
-    );
-});
+if (rootElement) {
+    const reactHydrateOrRender = rootElement.innerHTML === '' ? ReactDOM.render : ReactDOM.hydrate;
+
+    Loadable.preloadReady().then(() => {
+        reactHydrateOrRender(
+            React.createElement(IsomorphicApp, {
+                client: IsomorphicApolloClient.getClient(),
+            }),
+            rootElement,
+        );
+    });
+} else {
+    throw new Error('No element to render');
+}
