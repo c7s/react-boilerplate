cb82b4a501fcf79452024680f57079beb66bdec3
Leonid Fenko <fenok2112@gmail.com>
2019-06-18 17:26:08 +0300

    ORG-550: Update route replace logic (#121)

--- packages/frontend/src/client/modules/common/components/Link/Link.tsx
+++ packages/frontend/src/client/modules/common/components/Link/Link.tsx
@@ -1,4 +1,5 @@
 import React, { AnchorHTMLAttributes } from 'react';
+import { Location, LocationDescriptor, LocationDescriptorObject } from 'history';
 import { HashLink, HashLinkProps } from 'react-router-hash-link';
 import styled, { css, ThemeProvider } from 'styled-components';
 import theme from 'styled-theming';
@@ -37,7 +38,6 @@ const Link = React.forwardRef(
         ref: React.Ref<HTMLAnchorElement>,
     ) => {
         const { location } = useReactRouter();
-        const isSameLocation = to === (location.pathname || '') + (location.search || '') + (location.hash || '');
 
         return (
             <ThemeProvider theme={props.theme || DEFAULT_THEME}>
@@ -60,7 +60,7 @@ const Link = React.forwardRef(
                             (el => el.scrollIntoView({ behavior: smooth ? 'smooth' : 'auto', block: 'start' }))
                         }
                         to={to}
-                        replace={replace !== undefined ? replace : isSameLocation}
+                        replace={replace !== undefined ? replace : isSameLocation(location, to)}
                         innerRef={ref}
                         disabled={disabled}
                         tabIndex={disabled ? -1 : tabIndex} // Prevents disabled link from focusing and possible 'clicking'
@@ -110,5 +110,25 @@ const Anchor = styled.a`
 const StyledLink = styled(HashLink)`
     ${linkCss};
 `;
+
+function isSameLocation(location: Location, to: LocationDescriptor): boolean {
+    if (typeof to === 'string') {
+        return isSameLocationString(location, to);
+    }
+    return isSameLocationObject(location, to);
+}
+
+function isSameLocationString(location: Location, to: string): boolean {
+    return to === location.pathname + location.search + location.hash;
+}
+
+function isSameLocationObject(location: Location, to: LocationDescriptorObject) {
+    return (
+        (to.pathname || '') === location.pathname &&
+        (to.search || '') === location.search &&
+        (to.hash || '') === location.hash &&
+        to.state === location.state
+    );
+}
 
 export { Link, Props, ThemeMode };
