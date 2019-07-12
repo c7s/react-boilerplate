9c250d019bdd5cd9b0227580c3a14fc3490ec1da
Leonid Fenko <fenok2112@gmail.com>
2019-07-12 13:55:48 +0300

    ORG-550: Update frontend-server to consider optional SSR

--- packages/frontend-server/src/index.js
+++ packages/frontend-server/src/index.js
@@ -6,28 +6,33 @@ const app = express();
 
 try {
     /* eslint-disable global-require,import/no-unresolved */
-    const { default: serverRenderer, publicFiles } = require('frontend/dist/server.bundle.js');
+    const { default: serverRenderer, publicFiles, getStaticHtml } = require('frontend/dist/server.bundle.js');
     const reactLoadableStats = require('frontend/dist/react-loadable.json');
     /* eslint-enable */
 
-    // Create public files and serve them from '/' with correct headers
-    Object.values(publicFiles).forEach(({ path: filePath, content, contentType }) => {
-        try {
-            fs.writeFileSync(path.resolve(`./node_modules/frontend/dist/public${filePath}`), content);
-
-            app.use(filePath, (req, res, next) => {
-                res.header('Content-Type', contentType);
-                next();
-            });
-        } catch (error) {
-            console.warn(`[WARN] Unable to create file at ${filePath}`);
-            console.warn(error);
-        }
+    // Create public files in 'public' to serve from '/'
+    Object.values(publicFiles).forEach(({ path: filePath, content }) => {
+        fs.writeFileSync(path.resolve(`./node_modules/frontend/dist/public${filePath}`), content);
     });
+
+    // Ensure that index.html existence matches SSR status
+    if (process.env.REACT_BOILERPLATE_IS_DISABLE_SSR) {
+        fs.writeFileSync(
+            path.resolve('./node_modules/frontend/dist/public/index.html'),
+            getStaticHtml({ reactLoadableStats })
+        );
+    } else if (fs.existsSync(path.resolve('./node_modules/frontend/dist/public/index.html'))) {
+        fs.unlinkSync(path.resolve('./node_modules/frontend/dist/public/index.html'));
+    }
 
     app.use(express.static('./node_modules/frontend/dist/public'));
     app.use('/static', express.static('./node_modules/frontend/dist/static'));
-    app.use(serverRenderer({ reactLoadableStats }));
+
+    if (process.env.REACT_BOILERPLATE_IS_DISABLE_SSR) {
+        app.use('*', express.static('./node_modules/frontend/dist/public/index.html'));
+    } else {
+        app.use(serverRenderer({ reactLoadableStats }));
+    }
 } catch (error) {
     if (error.code !== 'MODULE_NOT_FOUND') {
         throw error;
